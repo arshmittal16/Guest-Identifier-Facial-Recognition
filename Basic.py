153ecd359aab06f0ee1ac1543736193627f4fb6d
arshmittal16 <arshmittal16@gmail.com>
2020-08-24 23:25:26 +0530

    Initial

---
+++ Basic.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'C:\\Users\\Arsh-PC\\Desktop\\Work\\Guest Identification\\Facial Detection\\Images'
+images =[]
+classNames = []
+li = os.listdir(path)
+print(li)
+
+for im in li:
+    current = cv2.imread(f'{path}/{im}')
+    images.append(current)
+    classNames.append(os.path.splitext(im)[0])
+print(classNames)
+
+def findEncode(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def attend(name):
+    with open('Attendance.csv', 'r+') as f:
+        att = f.readline()
+        namelist = []
+        print(att)
+        for line in att:
+            entry = line.split(',')
+            namelist.append(entry[0])
+        if name not in namelist:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name}, {dtString}')
+
+encodelist1 = findEncode(images)
+print('Encoding Complete')
+
+capture = cv2.VideoCapture(0)
+
+while True:
+    success, img = capture.read()
+    imgs = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
+
+    current = face_recognition.face_locations(imgs)
+    encode_current = face_recognition.face_encodings(imgs, current)
+
+    for encodingface, faceloc in zip(encode_current, current):
+        matches = face_recognition.compare_faces(encodelist1, encodingface)
+        facedis = face_recognition.face_distance(encodelist1, encodingface)
+        print(facedis)
+
+        matchindex = np.argmin(facedis)
+
+        if matches[matchindex]:
+            name = classNames[matchindex].upper()
+            print(name)
+            y1, x2, y2, x1 = faceloc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0,255,0), cv2.FILLED)
+            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_SIMPLEX,1, (255,255,255), 2)
+          ##  attend(name)
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
